e3085de8c2bfc5228f20e93827a801cbfc50ef4d
Pav <pavel-m@live.com>
2018-08-14 23:39:13 -0400

    Learning loops in vue.

--- vue-movie-db/src/App.vue
+++ vue-movie-db/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-    <Header :title="title"/>
+  <div id="app" v-if="status == 'Ready'">
+    <Header :title="title"  />
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>{{ hello }}</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <ul>
+      <li v-for="(item, index) in list">
+        {{index + 1}} {{item}}  
+      </li> 
+      <li v-for="person in people">
+        {{person.name}}  
+      </li> 
+      <li v-for="(value, key) in profile">
+        {{key}}: {{value}}  
+      </li> 
+    </ul> -->
+    <HelloWorld
+      msg="Welcome to Your Vue.js App"
+    />
+  </div>
+  <div v-else-if="status == 'Loading'">
+    Loading
+  </div>
+  <div v-else>
+    Error
   </div>
 </template>
 
@@ -16,7 +35,26 @@ export default {
   data() {
     return { 
       hello: 'Hello World',
-      title: 'Vue Movie DB'
+      title: 'Vue Movie DB',
+      isTrue: true,
+      status: "Ready"
+      // list: [
+      //   "Penguin",
+      //   "Turtle",
+      //   "Red Panda"
+      // ],
+      // people: [{
+      //   id: "1",
+      //   name: 'Val',
+      // }, {
+      //   id: "2",
+      //   name: 'Pav'
+      // }],
+      // profile: {
+      //   name: "Pav",
+      //   age: 25,
+      //   job: 'Developer'
+      // }
     };
   },
   methods: {
